0348524a8bdad1ea9c0195b8455e80b96c7013b5
Okarin1 <Okarin1024@gmail.com>
2021-04-17 20:12:15 +0800

    push

---
+++ .vuepress/config.js
@@ -0,0 +1,111 @@
+module.exports = {
+  "title": "送报少年",
+  "description": "EL PSY CONGROO!",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "文档",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "联系",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/Okarin1",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    subSidebar: 'auto'//在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+    ,
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "友链": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Okarin",
+    "authorAvatar": "/avatar.png",
+    "record": "备案",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+
+
+}
---
+++ blogs/博文/first.md
@@ -0,0 +1,16 @@
+---
+title: 这是我的第一个文章
+date: 2021-04-11
+sidebar: 'auto'
+categories:
+ - 博文
+tags:
+ - 日常
+---
+
+::: tip
+你好
+:::
+<!-- more -->
+
+## 这是我的第一篇文章
